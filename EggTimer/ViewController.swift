a0468b78b9de17c6a5750447891b8e302f3966a9
d-arevalo12 <diego.arevalo@improving.com>
2024-09-05 13:16:58 -0600

    Added progress bar and timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,21 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  let eggTimes: [String: Int] = ["Soft": 5, "Medium": 7, "Hard": 12]
+  @IBOutlet weak var timerTitleView: UILabel!
+  @IBOutlet weak var progressBar: UIProgressView!
+
+  let eggTimes: [String: Int] = ["Soft": 5, "Medium": 420, "Hard": 720]
+  var totalTime = 0
+  var secondsPassed = 0
 
   @IBAction func softnessSelector(_ sender: UIButton) {
-    print(sender.currentTitle!)
+    //print(sender.currentTitle!)
     let hardness = sender.currentTitle ?? "Soft"
 
-    switch hardness {
-    case "Soft":
-      print(eggTimes["Soft"] ?? 5)
-    case "Medium":
-      print(eggTimes["Medium"] ?? 7)
-    case "Hard":
-      print(eggTimes["Hard"] ?? 121)
-    default:
-      print("Errror")
+    let totalTime = eggTimes[hardness]!
+
+    countDownTimer(timerTime: totalTime)
+    //print(eggTime)
+  }
+
+  func countDownTimer(timerTime: Int) {
+    let countdown = Array(1...timerTime)
+
+    let timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+      print(countdown[self.secondsPassed])
+      self.secondsPassed += 1
+      let percentageProgress = Float(self.secondsPassed) / Float((countdown.last!))
+      self.progressBar.progress = Float(percentageProgress)
+
+      if self.secondsPassed >= countdown.count {
+        timer.invalidate()
+        self.timerTitleView.text = "Done"
+      }
     }
   }
+
+  // TODO: Add alarm sound when progress bar is completed
+
 }
